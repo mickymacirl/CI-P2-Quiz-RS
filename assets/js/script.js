ecf3954fbaaa7bea76654ed8a64ddbef4f24bb0d
mickymacirl <mickymacster@gmail.com>
2022-09-17 18:49:37 +0000

    Add js element selectors

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/* Selecting all the elements on the page. */
+const start = document.querySelector("#start");
+const quiz = document.querySelector("#quiz");
+const getQuestion = document.querySelector("#getQuestion");
+const getAnswerImage = document.querySelector("#answerImage");
+const getOptionOne = document.querySelector("#A");
+const getOptionTwo = document.querySelector("#B");
+const getOptionThree = document.querySelector("#C");
+const getOptionFour = document.querySelector("#D");
+const counter = document.querySelector("#counter");
+const questionTimer = document.querySelector("#questionTimer");
+const progress = document.querySelector("#progress");
+const diplayScore = document.querySelector("#scoreContainer");
